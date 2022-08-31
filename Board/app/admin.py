782ca1b5653e132dec5686923e68e7d36393a6e1
okSYS <ViacheslavDan803@gmail.com>
2022-08-31 17:20:19 +0300

    some edits user

--- Board/app/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from app.models import Profile
-
-admin.site.register(Profile)
---
+++ mysite/app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    """
+    Функция отображения для домашней страницы сайта.
+    """
+    # Генерация "количеств" некоторых главных объектов
+   
+    # Отрисовка HTML-шаблона index.html с данными внутри
+    # переменной контекста context
+    return render(
+        request,
+        'index.html',
+        context={},
+    )
