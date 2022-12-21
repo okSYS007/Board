7d7c53c53f0b7af7fa387ad372898e2169fb9b8e
okSYS <ViacheslavDan803@gmail.com>
2022-12-21 16:10:26 +0200

    some update filter qs

--- mysite/app/filters.py
+++ mysite/app/filters.py
@@ -2,9 +2,7 @@ from django_filters import FilterSet, AllValuesFilter
 from .models import Comments
  
 class PostFilter(FilterSet):
-
-    def __init__(self, *args, **kwargs):
-        super(PostFilter, self).__init__(*args, **kwargs)
-  
-    def filter(self, queryset, value):
-        return queryset
+        
+    class Meta:
+        model = Comments
+        fields = ['Announcement']
