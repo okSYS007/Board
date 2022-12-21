dfe80d251b2f1eef5314ed2855e4592dd547861c
okSYS <ViacheslavDan803@gmail.com>
2022-12-21 15:55:42 +0200

    filters fix

--- mysite/app/filters.py
+++ mysite/app/filters.py
@@ -3,15 +3,8 @@ from .models import Comments
  
 class PostFilter(FilterSet):
 
-    # Announcement = AllValuesFilter(field_name='Announcement', method='filter_published')
-
-    def filter_published(self, queryset, name, value):
-        # construct the full lookup expression.
-        lookup = '__'.join([name, 'isnull'])
-        return queryset.filter(**{lookup: True})
-
-        # alternatively, you could opt to hardcode the lookup. e.g.,
-        # return queryset.filter(published_on__isnull=False)
-    class Meta:
-        model = Comments
-        fields = ['Announcement']
+    def __init__(self, *args, **kwargs):
+        super(PostFilter, self).__init__(*args, **kwargs)
+  
+    def filter(self, queryset, value):
+        return queryset
